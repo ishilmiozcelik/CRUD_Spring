d818c9d0e5423423261872920c476abc8b483d2d
ismail h. ozcelik <iozcelyk@gmail.com>
2023-09-23 13:53:53 +0300

    controller

--- src/main/java/com/isozcelik/CRUD/controller/UserController.java
+++ src/main/java/com/isozcelik/CRUD/controller/UserController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class UserController {
 
     private final UserRepository userRepository;
-
     @Autowired
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
